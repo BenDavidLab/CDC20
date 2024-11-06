d6ab585e8bb8ba660748a1ee70bf1056308ec344
BenDavidLab <ubendavid@tauex.tau.ac.il>
2024-11-06 20:25:45 +0200

    Update anp_score_vs_drug_sensitivity.R

--- anp_score_vs_drug_sensitivity.R
+++ anp_score_vs_drug_sensitivity.R
@@ -1,5 +1,8 @@
 library(jtools)
 library(dplyr)
+library(stringr)
+library(effsize)
+library(xlsx)
 
 ###function declarations####
 get_partlize_cor <- function(data, outcome, v, covariat) {
@@ -31,28 +34,28 @@ get_partlize_cor <- function(data, outcome, v, covariat) {
                                                 unadj_ttest_pvalue = unadj_ttest_pvalue, unadj_ttest_cohens_d = unadj_ttest_cohens_d)))
 }
 ###drug name preprocessing####
-drug_info <- read.csv('data/primary-screen-replicate-treatment-info.csv')
+drug_info <- read.csv("data/Repurposing_Public_23Q2_Extended_Primary_Compound_List.csv")
 drugs <- c('MPI-0479605','AZ3146')
-drugs <- drug_info %>% dplyr::filter(tolower(name) %in% tolower(drugs)) %>% dplyr::select(name, broad_id) %>% unique()
+drugs <- drug_info %>% dplyr::filter(tolower(Drug.Name) %in% tolower(drugs)) %>% dplyr::select(name = Drug.Name, broad_id =IDs) %>% unique()
 
 ###drug sensitivity data####
-primary_screen_replicate_collapsed_logfold_change <- read.csv('data/primary-screen-replicate-collapsed-logfold-change.csv')
-colnames(primary_screen_replicate_collapsed_logfold_change)[1] <- 'DepMap_ID'
+primary_screen_23Q2_data <- read.csv("data/PRISM_Repurposing_Public_23Q2_subsetted.csv")
+colnames(primary_screen_23Q2_data)[1] <- 'DepMap_ID'
 for(i in 1:nrow(drugs)){
-  col_indx <- which(str_detect(colnames(primary_screen_replicate_collapsed_logfold_change), gsub('-','\\.',drugs$broad_id[i])))
-  colnames(primary_screen_replicate_collapsed_logfold_change)[col_indx] <- drugs$name[i]
+  col_indx <- which(str_detect(colnames(primary_screen_23Q2_data), gsub(':','\\.',gsub('-','\\.',drugs$broad_id[i]))))
+  colnames(primary_screen_23Q2_data)[col_indx] <- drugs$name[i]
 }
-drugs_sensitivity <- primary_screen_replicate_collapsed_logfold_change %>% select(DepMap_ID, any_of(drugs$name))
+drugs_sensitivity <- primary_screen_23Q2_data %>% select(DepMap_ID, any_of(drugs$name))
 
 ####expression data####
-expression <- read.csv('data/CCLE_expression.csv')
+expression <- read.csv("data/CCLE_expression.csv")
 colnames(expression) <- gsub('\\.\\..*','',colnames(expression))
 expression_zscore <- data.frame(scale(expression[,-1]))
 expression_zscore <- cbind(expression[,1], expression_zscore)
 colnames(expression_zscore)[1] <- 'DepMap_ID'
 
 ###aneuploidy data####
-aneuploidy_data <- read.csv('data/Depmap AS updated 1700.csv') %>% select(DepMap_ID, num_arm_events)
+aneuploidy_data <- read.csv("data/Depmap AS updated 1700.csv") %>% select(DepMap_ID, num_arm_events)
 
 ###merge data####
 cell_line_data <- merge(expression_zscore, drugs_sensitivity, by = 'DepMap_ID', all = TRUE)
@@ -60,13 +63,33 @@ cell_line_data <- merge(cell_line_data, aneuploidy_data, by = 'DepMap_ID', all =
 
 ###get partial correlation####
 gene = 'CDC20'
-if (!dir.exists('results/')) {
-  dir.create('results/')
-}
-for(treatment_name in drugs){
-  treatments_and_exp  <- dplyr::select(cell_line_data, DepMap_ID, num_arm_events, any_of(c(gene, treatment_name))) %>% na.omit() 
-  colnames(treatments_and_exp)[colnames(treatments_and_exp) == treatment_name] <- 'treatment'
-  x = get_partlize_cor(treatments_and_exp, 'treatment', 'num_arm_events', gene)
-  write.csv(x$data, paste0('results/',treatment_name,'_',gene,'_adjusted_drug_senstevity.csv'))
+
+if(!dir.exists('results/drug_sensitivity_vs_as')){
+  dir.create('results/drug_sensitivity_vs_as', recursive = TRUE)
 }
 
+# Loop over each drug to create Excel files
+for (i in 1:nrow(drugs)) {
+  # Get the drug name
+  drug_name <- drugs$name[i]
+  
+  # Prepare the data for partial correlation adjustment
+  treatments_and_exp <- dplyr::select(cell_line_data, DepMap_ID, num_arm_events, any_of(c(gene, drug_name))) %>% na.omit()
+  
+  # Rename the drug column to 'treatment' for compatibility with the function
+  colnames(treatments_and_exp)[colnames(treatments_and_exp) == drug_name] <- 'treatment'
+  
+  # Perform partial correlation adjustment
+  result <- get_partlize_cor(treatments_and_exp, 'treatment', 'num_arm_events', gene)
+  
+  # Prepare the data for saving
+  output_data <- data.frame(
+    DepMap_ID = result$data$DepMap_ID,
+    aneuploidy_score = result$data$num_arm_events,
+    drug_sensitivity_unadjusted = result$data$treatment,
+    drug_sensitivity_adjusted = result$data$adj_treatment
+  )
+  
+  # Save the results to an Excel file
+  write.xlsx(output_data, file = paste0("results/drug_sensitivity_vs_as/",drug_name, "_sensitivity_adjustment.xlsx"), row.names = FALSE)
+}
